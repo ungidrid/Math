6c73af679c3c7c56ab2a4f024a775f6b0e1ce904
Pashka2107 <pashka1201@gmail.com>
2018-01-28 17:23:22 +0200

    Nothing were done

--- MathC++/MathC++/Source.cpp
+++ MathC++/MathC++/Source.cpp
@@ -2,8 +2,7 @@
 
 int main()
 {
-	Matrix<double> m1{ {1,2,4},{2,3,4},{3,4,5} }, m2{ {1,2,5},{2,2,1},{1,5,3} };
-	LinearEquation<Fraction> le1{ Fraction(1,2),2,3 }, le2{2,5,Fraction(1,4)};
-	cout << 2 * m1 << m1 * 2 << m1.inversed() << m1.transposed() << m1.power(0) << m1.rank();
+	Matrix<Fraction> m{ {0,2,3},{1,2,1},{0,3,2} };
+	cout << m.rank() << m.determinant();
 	system("PAUSE");
 }
